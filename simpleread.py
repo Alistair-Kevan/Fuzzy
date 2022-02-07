5333495893538bc676b46deb622aca204c13aba3
Alistair Kevan <alikevan123@gmail.com>
2022-02-07 19:50:30 +0000

    got it acmo

--- simpleread.py
+++ simpleread.py
@@ -1,7 +1,9 @@
 import serial
 # Try this port. If I get nothing printing out, try '/dev/ttyAMA0'
 #ser = serial.Serial('/dev/ttyACM0', 9600)
-#ser = serial.Serial('/dev/ttyACM0', 9600)
+#ser = serial.Serial('/dev/ttyACM0', 9600)#
+#new comment
+
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 if __name__ == '__main__':
     ser.reset_input_buffer()
