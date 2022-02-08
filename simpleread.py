1552bf14da9ab52d5587409d8644ba448fe03642
Alistair Kevan <alikevan123@gmail.com>
2022-02-08 15:53:57 +0000

    kill comment

--- simpleread.py
+++ simpleread.py
@@ -2,7 +2,6 @@ import serial
 # Try this port. If I get nothing printing out, try '/dev/ttyAMA0'
 #ser = serial.Serial('/dev/ttyACM0', 9600)
 #ser = serial.Serial('/dev/ttyACM0', 9600)#
-#new comment
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 if __name__ == '__main__':
