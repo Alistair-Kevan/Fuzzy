32a13ad20550d771a5d094dce4cda8d728cdb893
Alistair Kevan <alikevan123@gmail.com>
2022-02-15 15:43:32 +0000

    plus one

--- i2c.py
+++ i2c.py
@@ -1,7 +1,7 @@
 import board
 import adafruit_lsm303_accel
-import adafruit_lis2mdl
+import adafruit_lsm303dlh_mag
 
 i2c = board.I2C()
+mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
-mag = adafruit_lis2mdl.LIS2MDL(i2c)
