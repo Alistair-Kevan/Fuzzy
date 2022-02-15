5a732b8570661c735ae5b5b494604ae8e243d3f3
Alistair Kevan <alikevan123@gmail.com>
2022-02-15 17:48:54 +0000

    plus one

--- i2c.py
+++ i2c.py
@@ -7,7 +7,7 @@ import adafruit_lsm303dlh_mag
 i2c = busio.I2C(board.SCL, board.SDA)
 mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
 accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
-
+GPIO.setmode(GPIO.BOARD)
 while True:
     print("Acceleration (m/s^2): X=%0.3f Y=%0.3f Z=%0.3f"%accel.acceleration)
     print("Magnetometer (micro-Teslas)): X=%0.3f Y=%0.3f Z=%0.3f"%mag.magnetic)
