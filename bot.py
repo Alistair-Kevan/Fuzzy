8ca38d85c76816ff047e237f362fea7e72fb8605
Alistair Kevan <alikevan123@gmail.com>
2022-02-17 15:03:49 +0000

    plus one

--- bot.py
+++ bot.py
@@ -45,27 +45,7 @@ def get_heading(_sensor):
     magnet_x, magnet_y, _ = _sensor.magnetic
     return vector_2_degrees(magnet_x, magnet_y)
 
-
-
-
-
 def setup():
-    # Use GPIO numbers not pin numbers
-
-    #GPIO.setmode(GPIO.BOARD)
-
-
-    # set up the GPIO channels - one input and one output
-    """GPIO.setup(40, GPIO.OUT)
-    GPIO.setup(38, GPIO.OUT)
-    GPIO.setup(36, GPIO.OUT)
-    GPIO.setup(32, GPIO.OUT)
-
-    GPIO.setup(26, GPIO.OUT)
-    GPIO.setup(24, GPIO.OUT)
-    GPIO.setup(35, GPIO.OUT)
-    GPIO.setup(37, GPIO.OUT)
-    """
 
 
 def loop():
@@ -75,7 +55,7 @@ def loop():
         print("heading: {:.2f} degrees".format(head))
         print(head)
         if 5 < head <= 180.0:
-            print("turn right")#from high numbers towards north
+            print("turn left")#from high numbers towards north
             fr1.value = 1
             fr2.value = 0
             br1.value = 1
@@ -87,7 +67,7 @@ def loop():
             bl2.value = 1
 
         elif 180 < head < 355.0:
-            print("turn left")  # from low numbers towards north
+            print("turn right")  # from low numbers towards north
             fr1.value = 0
             fr2.value = 1
             br1.value = 0
@@ -110,28 +90,12 @@ def loop():
             bl2.value = 0
         time.sleep(0.01)
 
-        # output to GPIO8
-        # rightfw
-
 
 
 
 def destroy():
-    # pwm.stop()
-    # rightfw
     RPi.GPIO.cleanup()
     print("\nCleaned up GPIO resources.")
-    """GPIO.output(35, 0)
-    GPIO.output(37, 0)
-
-    GPIO.output(40, 0)
-    GPIO.output(38, 0)
-    # leftfw
-    GPIO.output(26, 0)
-    GPIO.output(24, 0)
-    GPIO.output(36, 0)
-    GPIO.output(32, 0)
-    GPIO.cleanup()"""
 
 
 if __name__ == '__main__':
