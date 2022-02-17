941445b1cb64daf3bfcf2144db923f0776c77eb8
Alistair Kevan <alikevan123@gmail.com>
2022-02-17 17:08:57 +0000

    go

--- us.py
+++ us.py
@@ -25,6 +25,7 @@ while True:
     try:
         print((sonarfl.distance,),(sonarfm.distance,),(sonarfr.distance,))
         print((sonarbl.distance,), (sonarbm.distance,), (sonarbr.distance,))
+        print("")
     except RuntimeError:
         print("Retrying!")
     except KeyboardInterrupt:
