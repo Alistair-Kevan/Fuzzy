f57ee89d09df7438c98646f0ea9bdbd11b5360f1
Alistair Kevan <alikevan123@gmail.com>
2022-02-17 17:17:11 +0000

    go

--- us.py
+++ us.py
@@ -8,7 +8,7 @@ import RPi.GPIO
 import board
 import digitalio
 
-
+RPi.GPIO.cleanup()
 sonarfl = adafruit_hcsr04.HCSR04(trigger_pin=board.D9, echo_pin=board.D11)#9, 11
 sonarfm = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 sonarfr = adafruit_hcsr04.HCSR04(trigger_pin=board.D22, echo_pin=board.D10)#22, 10
@@ -17,9 +17,11 @@ sonarbr = adafruit_hcsr04.HCSR04(trigger_pin=board.D17, echo_pin=board.D27)#17,2
 sonarbm = adafruit_hcsr04.HCSR04(trigger_pin=board.D24, echo_pin=board.D25)#24,25
 sonarbl = adafruit_hcsr04.HCSR04(trigger_pin=board.D18, echo_pin=board.D23)#18, 23
 
+
 def destroy():
     RPi.GPIO.cleanup()
     print("\nCleaned up GPIO resources.")
+
 
 while True:
     try:
@@ -31,6 +33,7 @@ while True:
     except KeyboardInterrupt:
         destroy()
     time.sleep(0.1)
+
 
 
 
