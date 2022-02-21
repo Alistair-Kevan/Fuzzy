0707b469ecc118a10e1bb5e9bbdfd816352f13b3
Alistair Kevan <alikevan123@gmail.com>
2022-02-21 11:31:47 +0000

    go

--- us.py
+++ us.py
@@ -9,14 +9,14 @@ import board
 import digitalio
 
 RPi.GPIO.cleanup()
-sonarfl = adafruit_hcsr04.HCSR04(trigger_pin=board.D9, echo_pin=board.D11)#9, 11
+'''sonarfl = adafruit_hcsr04.HCSR04(trigger_pin=board.D9, echo_pin=board.D11)#9, 11
 sonarfm = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 sonarfr = adafruit_hcsr04.HCSR04(trigger_pin=board.D22, echo_pin=board.D10)#22, 10
 
 sonarbr = adafruit_hcsr04.HCSR04(trigger_pin=board.D17, echo_pin=board.D27)#17,27
 sonarbm = adafruit_hcsr04.HCSR04(trigger_pin=board.D24, echo_pin=board.D25)#24,25
 sonarbl = adafruit_hcsr04.HCSR04(trigger_pin=board.D18, echo_pin=board.D23)#18, 23
-
+'''
 
 def destroy():
     RPi.GPIO.cleanup()
