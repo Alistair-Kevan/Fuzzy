5a234c0fa8f0fd78183fb236d80269d095eee0b9
Alistair Kevan <alikevan123@gmail.com>
2022-02-21 11:32:40 +0000

    go

--- us.py
+++ us.py
@@ -42,7 +42,7 @@ with HCSR04(trigger_pin=board.D5, echo_pin=board.D6) as sonar:
     try:
         while True:
             print(sonar.distance)
-            sleep(2)
+            time.sleep(2)
     except KeyboardInterrupt:
         pass
 
