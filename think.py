80620cbb1a8f6d4c01692d6a6830889c2607a718
Alistair Kevan <alikevan123@gmail.com>
2022-03-02 17:51:56 +0000

    go

--- think.py
+++ think.py
@@ -95,7 +95,7 @@ def loop():
         head = get_heading(sensor)
         print("heading: {:.2f} degrees".format(head))
         roomhead = headchange(head, roomofset)#alighnes heading to room
-        print("roomhead: ", roomhead)
+        print("roomhead: ", roomhead, "roomofset", roomofset)
         #print(head)
         try:
             bm = sonarbm.distance
