d7f54f2fdec16bcaa213bc63215f0beb13616c14
Alistair Kevan <alikevan123@gmail.com>
2022-03-11 15:59:41 +0000

    go

--- fuzzy.py
+++ fuzzy.py
@@ -111,10 +111,15 @@ def x_close(x): # /T shape right end
         return (fallend - x) / (fallend - fallstart)#falling edge
 
 
-def leftmotorpwm(speed):
-    fl1.value = 1
+def motors(leftcycle, rightcycle):
+    fr1.duty_cycle = rightcycle
+    fr2.value = 0
+    br1.duty_cycle = rightcycle
+    br2.value = 0
+
+    fl1.duty_cycle = leftcycle
     fl2.value = 0
-    bl1.value = 1
+    bl1.duty_cycle = leftcycle
     bl2.value = 0
 
 
@@ -179,16 +184,8 @@ def loop():
         import board
         #fr1.duty_cycle = 2 ** 15
         for cycle in range(0, 65535):  # Cycles through the full PWM range from 0 to 65535
-            #print("go!")
-            fr1.duty_cycle = cycle
-            fr2.value = 0
-            br1.duty_cycle = cycle
-            br2.value = 0
 
-            fl1.duty_cycle = cycle
-            fl2.value = 0
-            bl1.duty_cycle = cycle
-            bl2.value = 0
+
         # = pwmio.PWMOut(board.LED)
         #pwm.duty_cycle = 2 ** 15
         #time.sleep(0.1)
